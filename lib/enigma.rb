72d901d7a549700a34558163da11a5a6ecfc9251
Gvieve <gnuebel84@yahoo.com>
2021-01-16 11:14:07 -0700

    Create test and initial code to generate offset with todays date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,28 @@
+require 'date'
+
 class Enigma
-  attr_reader :key
+  attr_reader :key,
+              :offset
 
   def initialize
     @key = 0
+    @offset = 0
   end
 
   def generate_random_key
     numbers = Array(0..9)
     @key = numbers.sample(5).join.to_i
+  end
+
+  def numerical_date
+    Date.today.strftime("%d%m%y").to_i
+  end
+
+  def date_squared
+    (numerical_date * numerical_date).to_s
+  end
+
+  def generate_offset
+    @offset = date_squared[-4..-1].to_i
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require_relative './test_helper'
 require './lib/enigma'
 
@@ -15,8 +16,18 @@ class EnigmaTest < Minitest::Test
 
     assert_equal 0, enigma.key
     enigma.generate_random_key
-    # enigma.stubs(:key).returns(57145)
+    Enigma.any_instance.stubs(:key).returns(57145)
     #
     assert_equal 57145, enigma.key
+  end
+
+  def test_it_can_use_todays_date_for_offset
+    enigma = Enigma.new
+
+    assert_equal 0, enigma.offset
+    assert_equal 160121, enigma.numerical_date
+    assert_equal "25638734641", enigma.date_squared
+    enigma.generate_offset
+    assert_equal 4641, enigma.offset
   end
 end
