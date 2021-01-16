d9afe8ed8f619429f2b19cc712dec89187d027d7
Gvieve <gnuebel84@yahoo.com>
2021-01-16 16:44:38 -0700

    Update encrypt optional args to directly call generate key/date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,46 +1,20 @@
 require 'date'
 
 class Enigma
-  attr_reader :message,
-              :key,
-              :date
 
-  def encrypt(message, key = nil, date = nil)
+  def encrypt(message, key = generate_key, date = generate_date)
+
     {:encryption => message,
-     :key => store_key(key),
-     :date => store_date(key, date)}
-  end
-
-  def store_key(key)
-    if key.nil?
-      key = generate_key
-    elsif key.length == 5
-      key = key
-    elsif key.length == 6
-      key = generate_key
-    end
-  end
-
-  def store_date(key, date)
-    if date.nil? && key.nil?
-      date = generate_date
-    elsif date.nil? && key.length == 6
-      date = key
-    elsif date.nil? && key.length == 5
-      date = generate_date
-    elsif date.length == 6
-      date = date
-    end
+     :key => key,
+     :date => date}
   end
 
   def generate_key
-    numbers = Array(0..9)
-    numbers.sample(5).join
+    (Array(0..9)).sample(5).join
   end
 
   def generate_date
-    date = Date.today
-    date.strftime("%d%m%y")
+    Date.today.strftime("%d%m%y")
   end
 
   # date_squared(date)[-4..-1].to_i
