92c992aa17722d528a66d5112e3f2690df88069f
Gvieve <gnuebel84@yahoo.com>
2021-01-16 18:07:21 -0700

    Add test to validate if encrypt args are valid

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,17 +13,20 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypt_accepts_key_and_date_arguments
+    skip
     enigma = Enigma.new
     message = "hello world"
     key = "02715"
     date = "040895"
 
+    assert_equal Hash, enigma.encrypt(message, key, date).class
     assert_equal "hello world", enigma.encrypt(message, key, date)[:encryption]
     assert_equal "02715", enigma.encrypt(message, key, date)[:key]
     assert_equal "040895", enigma.encrypt(message, key, date)[:date]
   end
 
   def test_encrypt_accepts_has_key_no_date
+    skip
     enigma = Enigma.new
     message = "hello world"
     key = "02715"
@@ -36,6 +39,7 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_encrypt_accepts_no_key_no_date
+    skip
     enigma = Enigma.new
     message = "hello world"
     Enigma.any_instance.stubs(:generate_key).returns("57145")
@@ -55,5 +59,40 @@ class EnigmaTest < Minitest::Test
 
     Enigma.any_instance.stubs(:generate_key).returns("57145")
     assert_equal "57145", enigma.generate_key
+  end
+
+  def test_generate_date
+    enigma = Enigma.new
+    date = Date.today.strftime("%d%m%y")
+
+    assert_equal 6, enigma.generate_date.length
+    assert_equal String, enigma.generate_date.class
+    assert_equal date, enigma.generate_date
+  end
+
+  def test_it_has_valid_inputs?
+    enigma = Enigma.new
+    valid_msg = "hello world"
+    valid_key = "02715"
+    valid_date = "040895"
+
+    assert_equal true, enigma.valid_input?(valid_msg, valid_key, valid_date)
+
+    invalid_msg = 231
+    invalid_key = 775241
+    invalid_date = "040895"
+    assert_equal false, enigma.valid_input?(invalid_msg, invalid_key, invalid_date)
+  end
+
+  def test_is_valid_message?
+    enigma = Enigma.new
+    valid = "Test"
+    invalid1 = 1234
+    invalid2 = nil
+
+    # assert_equal true, enigma.valid_message?(valid)
+    assert_equal true, enigma.valid_message?(valid)
+    assert_equal false, enigma.valid_message?(invalid1)
+    assert_equal false, enigma.valid_message?(invalid2)
   end
 end
