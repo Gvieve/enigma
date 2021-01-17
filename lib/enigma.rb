e952f3aa9c975bc795ef7addf5a6c487bcbfdbb3
Gvieve <gnuebel84@yahoo.com>
2021-01-16 22:19:55 -0700

    Add offset method code

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -41,21 +41,9 @@ class Enigma
     Date.today.strftime("%d%m%y")
   end
 
-  # date_squared(date)[-4..-1].to_i
-  #
-  # def numerical_date(date = Date.today)
-  #   # require "pry"; binding.pry
-  #   if date.class == String
-  #     date
-  #   else
-  #     date.strftime("%d%m%y")
-  #   end
-  # end
-  #
-  # def date_squared(date)
-  #   # require "pry"; binding.pry
-  #   ((numerical_date(date).to_i) * (numerical_date(date).to_i)).to_s
-  # end
-  #
+  def offset(date)
+    ((date.to_i) * (date.to_i)).to_s[-4..-1]
+  end
+  ## date_squared(date)[-4..-1].to_i
   #
 end
